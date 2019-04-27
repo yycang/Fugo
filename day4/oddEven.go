b61e640251095265362dc545a2459f854e4883f7
yycang <cyy_dfh@163.com>
2019-04-27 21:17:35 +0800

    递归判断奇数偶数

---
+++ day4/oddEven.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d is even : %t\n", 5, even(5))
+	fmt.Printf("%d is odd : %t\n", 6, odd(6))
+	fmt.Printf("%d is odd : %t\n", 7, odd(7))
+}
+
+func even(n int) bool {
+	if n == 0 {
+		return true
+	}
+	return odd(RevSign(n) - 1)
+}
+
+func odd(n int) bool  {
+	if n == 0 {
+		return false
+	}
+	return even(RevSign(n) - 1)
+}
+
+func RevSign(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
