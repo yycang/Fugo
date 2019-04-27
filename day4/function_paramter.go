8ec6a5ae16540982fc197f05fab5e5bff6292ef9
yycang <cyy_dfh@163.com>
2019-04-27 21:47:12 +0800

    参数函数

---
+++ day4/function_paramter.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main()  {
+	callback(1, Add)
+}
+
+func Add(a, b int)  {
+	fmt.Printf("%d + %d = %d%n", a, b, a+b)
+}
+
+func callback(y int, f func(int, int)) {
+	f(y, 2)
+}
