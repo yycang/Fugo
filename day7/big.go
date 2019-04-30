73b804e8519e1f0098132f5878617a8ea9ba615b
yycang <cyy_dfh@163.com>
2019-04-30 12:11:36 +0800

    精密计算

---
+++ day7/big.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+)
+
+// big.NewInt()构造大的整型数字
+// bing.NewRat()构造大有理数
+func main() {
+	im := big.NewInt(math.MaxInt64)
+	in := im
+	io := big.NewInt(1956)
+	ip := big.NewInt(1)
+	ip.Mul(im, in).Add(ip, im).Div(ip, io)
+	fmt.Printf("big int %v\n", ip)
+
+	rm := big.NewRat(math.MaxInt64, 1956)
+	rn := big.NewRat(-1956, math.MaxInt64)
+	ro := big.NewRat(19, 56)
+	rp := big.NewRat(1111, 2222)
+	rq := big.NewRat(1, 1)
+	rq.Mul(rm, rn).Add(rq, ro).Mul(rq, rp)
+
+	fmt.Printf("big rat %v\n", rq)
+}
