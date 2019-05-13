5a835f9aa3829ca9479f4019d8d599374bdb24e9
yycang <cyy_dfh@163.com>
2019-05-13 23:33:18 +0800

    读取压缩文件

---
+++ day13/gzipped.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fName := "myfile.gz"
+	var r *bufio.Reader
+	fi, err := os.Open(fName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v cant open this %s! error: %s\n", os.Args[0], fName, err)
+		os.Exit(1)
+	}
+	defer fi.Close()
+	fz, err := gzip.NewReader(fi)
+	if err != nil {
+		r = bufio.NewReader(fi)
+	} else {
+		r = bufio.NewReader(fz)
+	}
+
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			fmt.Println("done reading file")
+			os.Exit(0)
+		}
+		fmt.Println(line)
+	}
+}
