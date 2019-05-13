ebd335e26674e65a51a5cf5f37a503c6ff12b8d0
yycang <cyy_dfh@163.com>
2019-05-13 22:58:33 +0800

    ioutil读文件方法

---
+++ day13/read_write_file.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	inputFile := "products.txt"
+	outputFile := "prodects_copy.txt"
+
+	buf, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "file error: %s\n", err)
+	}
+
+	fmt.Printf("%s\n", string(buf))
+	err = ioutil.WriteFile(outputFile, buf, 0644)
+	if err != nil {
+		panic(err.Error())
+	}
+}
