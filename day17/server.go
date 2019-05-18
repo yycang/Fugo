451d050b859e34ce413b57a42fbfcfe3ad9926a2
yycang <cyy_dfh@163.com>
2019-05-18 13:10:47 +0800

    tcp服务端

---
+++ day17/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("starting the server")
+	listener, err := net.Listen("tcp", "localhost:50000")
+
+	if err != nil {
+		fmt.Println("error", error.Error())
+		return
+	}
+	// 监听并接受客户端链接
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error accepting", err.Error())
+			return
+		}
+		go doServerStuff(conn)
+	}
+}
+
+func doServerStuff(conn net.Conn)  {
+	for {
+		// 使用512字节缓冲data来读取客户端发送的数据
+		buf := make([]byte, 512)
+		len, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("error reading", err.Error())
+			return
+		}
+		fmt.Printf("received data: %v", string(buf[:len]))
+	}
+}
