d8ae860e17d23afaef9659d0694c0e23f55bf4e0
yycang <cyy_dfh@163.com>
2019-06-01 22:55:08 +0800

    映射用法

---
+++ day19/about_map.go
@@ -0,0 +1,15 @@
+// 创建映射
+map1 := make(map[keytype] valuetype)
+map2 := map[string]int{"one":1, "two":2}
+
+// 遍历映射
+for key, value := range map1 {
+
+}
+
+// 检测key是否存在
+val1, isPresent = map1[key]
+
+// 映射删除键
+delete(map1, key)
+
