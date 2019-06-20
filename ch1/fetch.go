741cd3b5bf10416b0140bca0a7e4dcb8329e2755
yycang <cyy_dfh@163.com>
2019-06-20 15:50:05 +0800

    1.5获取一个url

---
+++ ch1/fetch.go
@@ -0,0 +1,28 @@
+// 输出从url中获取的内容
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		//b, err := ioutil.ReadAll(resp.Body)
+		b, err := io.Copy(os.Stdin, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
