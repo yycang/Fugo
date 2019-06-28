325cc2452a86b6c5c7fe93e1c7dab57c0b441273
yycang <cyy_dfh@163.com>
2019-06-28 23:59:03 +0800

    2.6包和文件

---
+++ ch2/tempconv/tempconv.go
@@ -0,0 +1,25 @@
+// 进行摄氏温度和华式温度的转换计算
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC Celsius = 0
+	BoilingC Celsius = 100
+)
+
+func (c Celsius) String() string  {
+	return fmt.Sprintf("%gºC", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%gºF", f)
+}
+
+func CToF(c Celsius) Fahrenheit {return Fahrenheit(c * 9 / 5 + 32)}
+func FToC(f Fahrenheit) Celsius {return Celsius((f - 32 * 5 / 9))}
+
